c060c8859ca34a6764b3cd090fa3d0ebf46bc07a
EnzoDelloso <159044790+EnzoDelloso@users.noreply.github.com>
2024-09-05 12:50:26 +0000

    HA, FA, Adder2bits, e parte do adder implementados

--- ula_modules.py
+++ ula_modules.py
@@ -11,6 +11,8 @@ from myhdl import *
 def halfAdder(a, b, soma, carry):
     @always_comb
     def comb():
+        carry.next = a and b
+        soma.next = (not a and b) or (a and not b)
         pass
 
     return instances()
@@ -18,21 +20,36 @@ def halfAdder(a, b, soma, carry):
 
 @block
 def fullAdder(a, b, c, soma, carry):
+    s = [Signal(bool(0)) for i in range(3)]
+    haList = [None for i in range(2)]  # (1)
+
+    haList[0] = halfAdder(a, b, s[0], s[1]) 
+    haList[1] = halfAdder(c, s[0], soma, s[2])
+
     @always_comb
     def comb():
-        pass
+        carry.next = s[1] | s[2]
 
     return instances()
 
 
+
 @block
 def adder2bits(x, y, soma, carry):
+    c = Signal(bool(0))
+    ha = halfAdder(x[0], y[0], soma[0], c)
+    fa = fullAdder(x[1], y[1], c,  soma[1], carry )
     return instances()
 
 
 @block
 def adder(x, y, soma, carry):
-    @always_comb
+    n = len(x)
+    c = [Signal(bool(0)) for i in range(n + 1)]
+    faList = [None for i in range(n)]
+    
+    for i in range(n):
+        faList[i] = fullAdder(x[i], y[i], c[i], soma[i], c[i - 1])
     def comb():
         pass
 
